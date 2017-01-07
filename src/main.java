11623ad2977c2ea82cbf825de8e128b3c2774580
Jana Kindlam <janakindl@gmail.com>
2017-01-08 00:34:32 +0200

    change name

--- src/main.java
+++ src/main.java
@@ -2,6 +2,8 @@ public class Main {
 	public static void main(String[] args) {
         // Prints "Alustame ..." to the terminal window.
         System.out.println("Alustame pommitamisega");
+        System.out.println("Autor: Jana");
+        System.out.println("Aasta: 2017");
     }
 
 }
