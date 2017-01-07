19f94d7be9669f7adf4ee625c341181b3829c256
Jana Kindlam <janakindl@gmail.com>
2017-01-07 23:16:08 +0200

    prooov 1

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class Main { 
+	public static void main(String[] args) {
+        // Prints "Alustame ..." to the terminal window.
+        System.out.println("Alustame pommitamisega");
+    }
+
+}
